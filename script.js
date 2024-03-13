abcf8e31f830312ec0313b6a51a19c95f32d7d29
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-03-13 11:46:36 -0300

    alterações

--- script.js
+++ script.js
@@ -30,45 +30,23 @@ async function FetchApi(query) {
 mensagem.innerHTML = (`<p>Agora são ${hora} horas e ${minutos} minutos</p>`)
 
 if (hora > 5 && hora <= 12) {
-   let source = FetchApi('morning');
+   
    titulo.innerHTML += ("&#9788;Hora do Dia&#9788;")
    mensagem.innerHTML += ("<p>Bom dia!</p>")
-   imagem.src = source;
+   imagem.src = FetchApi('morning');
    corpo.style.backgroundColor = "#FFEC5C"
 }
 else if (hora > 12 && hora <= 18) {
-  let source = FetchApi('afternoon');
   titulo.innerHTML += ("Hora do Dia");
   mensagem.innerHTML += ("<p>Boa tarde!</p>");
-  imagem.src = source;
+  imagem.src = FetchApi('afternoon');
   corpo.style.backgroundColor = "#9C3E00";
 }
 else {
-  let source = FetchApi('night');
+  
   titulo.innerHTML += ("&#10025;Hora do Dia&#10025;")
   mensagem.innerHTML += ("<p>Boa noite!</p>");
-  imagem.src = source;
+  imagem.src = FetchApi('night');
   corpo.style.backgroundColor = "#012030";
   
 }
-
-
-
-  
-  
-    
-/*
-const apiKey = 'tjsS7xQHjFZghfxXsgz8pKyTiNjG8tfm43HcauJCKMixPjdl5KWX9pji';
-const query = 'night';
-
-fetch(`https://api.pexels.com/v1/search?query=${query}`, {
-  headers: {
-    Authorization: apiKey
-  }
-})
-.then(response => response.json())
-.then(data => {
-  console.log(data['photos']); // Verifique o conteúdo da resposta
-})
-.catch(error => console.error('Erro ao fazer solicitação:', error));
-  */ 
