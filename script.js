05400b42c8c48429a2014083a6fd6e164426ac53
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-03-19 12:20:46 -0300

    alterações

--- script.js
+++ script.js
@@ -17,36 +17,37 @@ let options = {
 
 
 async function FetchApi(query) {
-  let random = Math.floor(Math.random() * 15);
   let url = `https://api.pexels.com/v1/search?query=${query}&limit=50`;
   const ApiResponse = await fetch(url, options);
   const data = await ApiResponse.json();
-  return data['photos'][random]['src']['portrait'];
+  return data;
   }
 
+  async function FetchImage(query) {
+    let random = Math.floor(Math.random() * 15);
+    const data = await FetchApi(query);
+    imagem.src = data['photos'][random]['src']['medium'];
+  }
 
-  console.log(FetchApi('night'));
   
 mensagem.innerHTML = (`<p>Agora são ${hora} horas e ${minutos} minutos</p>`)
 
 if (hora > 5 && hora <= 12) {
-   
    titulo.innerHTML += ("&#9788;Hora do Dia&#9788;")
    mensagem.innerHTML += ("<p>Bom dia!</p>")
-   imagem.src = FetchApi('morning');
+   FetchImage('day');
    corpo.style.backgroundColor = "#FFEC5C"
 }
 else if (hora > 12 && hora <= 18) {
   titulo.innerHTML += ("Hora do Dia");
   mensagem.innerHTML += ("<p>Boa tarde!</p>");
-  imagem.src = FetchApi('afternoon');
+  FetchImage('afternoon');
   corpo.style.backgroundColor = "#9C3E00";
 }
 else {
-  
   titulo.innerHTML += ("&#10025;Hora do Dia&#10025;")
   mensagem.innerHTML += ("<p>Boa noite!</p>");
-  imagem.src = FetchApi('night');
+  FetchImage('night');
   corpo.style.backgroundColor = "#012030";
   
 }
