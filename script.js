f16d9254f66c141e3ae991584185e43d02acc811
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2024-04-18 13:08:45 -0300

    alterações

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var hora = new Date().getHours()
-var minutos = new Date().getMinutes()
-var mensagem = document.getElementById ("msg")
-var imagem = document.getElementById ("foto")
-var titulo = document.getElementById ("title")
-let som = document.getElementById("som");
-
+let hora = new Date().getHours()
+let minutos = new Date().getMinutes()
+let mensagem = document.getElementById ("msg")
+let imagem = document.getElementById ("foto")
+let main = document.getElementById("conteudo")
+let titulo = document.getElementById ("title")
+let corpo = document.getElementById("corpo")
 
 let key = "tjsS7xQHjFZghfxXsgz8pKyTiNjG8tfm43HcauJCKMixPjdl5KWX9pji";
 let options = {
@@ -14,7 +14,6 @@ let options = {
     'Content-Type': 'application/json'
   }
 }
-
 
 async function FetchApi(query) {
   let url = `https://api.pexels.com/v1/search?query=${query}`;
@@ -28,28 +27,29 @@ async function FetchApi(query) {
   async function FetchImage(query) {
     let random = Math.floor(Math.random() * 15);
     const data = await FetchApi(query);
-    imagem.src = data['photos'][random]['src']['medium'];
+    corpo.style.background = `url(${data['photos'][random]['src']['original']})`;
+    corpo.style.backgroundRepeat = 'no-repeat';
+    corpo.style.backgroundSize = 'cover';
+    corpo.style.backgroundPosition = 'center center'
   }
 
-  
-mensagem.innerHTML = (`<p>Agora são ${hora} horas e ${minutos} minutos</p>`)
+  mensagem.innerHTML = (`<p>Agora são ${hora} horas e ${minutos} minutos</p>`)
 
-if (hora > 5 && hora <= 12) {
-   titulo.innerHTML += ("&#9788;Hora do Dia&#9788;")
-   mensagem.innerHTML += ("<p>Bom dia!</p>")
-   FetchImage('day');
-   corpo.style.backgroundColor = "#FFEC5C"
-}
-else if (hora > 12 && hora <= 18) {
-  titulo.innerHTML += ("Hora do Dia");
-  mensagem.innerHTML += ("<p>Boa tarde!</p>");
-  FetchImage('afternoon');
-  corpo.style.backgroundColor = "#9C3E00";
-}
-else {
-  titulo.innerHTML += ("&#10025;Hora do Dia&#10025;")
-  mensagem.innerHTML += ("<p>Boa noite!</p>");
-  FetchImage('night');
-  corpo.style.backgroundColor = "#012030";
-  
-}
+  if (hora > 5 && hora <= 12) {
+     titulo.innerHTML += ("&#9788;Hora do Dia&#9788;")
+     mensagem.innerHTML += ("<p>Bom dia!</p>")
+     FetchImage('day');
+  }
+  else if (hora > 12 && hora <= 18) {
+    titulo.innerHTML += ("Hora do Dia");
+    mensagem.innerHTML += ("<p>Boa tarde!</p>");
+    FetchImage('afternoon');
+    
+  }
+  else {
+    titulo.innerHTML += ("&#10025;Hora do Dia&#10025;")
+    mensagem.innerHTML += ("<p>Boa noite!</p>");
+    FetchImage('night');
+    main.style.backgroundColor = 'rgba(245, 245, 220, 0.671)'
+  }
+
